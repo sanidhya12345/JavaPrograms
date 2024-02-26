a800586faa43fefb123873bc19eae489825e13b0
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-02-26 21:12:17 +0530

    java

---
+++ src/Sumoftwo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sumoftwo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int num1=sc.nextInt();
+        System.out.println(num+num1);
+    }
+}
